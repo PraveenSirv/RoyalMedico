38aff549fa0b659f32a5bf4044c264e71f82299e
Praveen Choudhary <160106353+PraveenSirv@users.noreply.github.com>
2026-05-06 12:25:13 +0530

    Delete inventory-service directory

--- inventory-service/src/main/java/com/royal_medical/inventory_service/exception/MedicineNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.royal_medical.inventory_service.exception;
-
-public class MedicineNotFoundException extends RuntimeException {
-    public MedicineNotFoundException(String message) {
-        super(message);
-    }
-}
