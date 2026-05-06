27fb25d8d67d7a87621e21f18e3b6aab23956a68
Praveen Choudhary <160106353+PraveenSirv@users.noreply.github.com>
2026-05-06 12:23:10 +0530

    Delete admin-service directory

--- admin-service/src/main/java/com/royal_medical/admin_service/exception/AdminNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.royal_medical.admin_service.exception;
-
-public class AdminNotFoundException extends RuntimeException {
-    public AdminNotFoundException(String message) {
-        super(message);
-    }
-}
