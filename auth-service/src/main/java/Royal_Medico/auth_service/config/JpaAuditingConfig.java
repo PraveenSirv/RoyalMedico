91a5f24e2fc5334856e27546875046d60ea5042f
Praveen Choudhary <160106353+PraveenSirv@users.noreply.github.com>
2026-05-06 15:07:39 +0530

    Delete auth-service directory

--- auth-service/src/main/java/Royal_Medico/auth_service/config/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package Royal_Medico.auth_service.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfig {
-}
--- auth-service/src/main/java/Royal_Medico/auth_service/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package Royal_Medico.auth_service.entity;
-
-public enum Role {
-    ADMIN,
-    PHARMACIST,
-    CUSTOMER
-}
