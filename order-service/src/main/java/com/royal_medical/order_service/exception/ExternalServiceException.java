664e31bfdcfa43747da5f40b823691cc0e243cf5
Praveen Choudhary <160106353+PraveenSirv@users.noreply.github.com>
2026-05-06 12:25:33 +0530

    Delete order-service directory

--- order-service/src/main/java/com/royal_medical/order_service/exception/ExternalServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.royal_medical.order_service.exception;
-
-public class ExternalServiceException extends RuntimeException {
-    public ExternalServiceException(String s) {
-        super(s);
-    }
-}
