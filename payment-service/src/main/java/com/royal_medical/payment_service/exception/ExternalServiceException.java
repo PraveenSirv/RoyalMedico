5976939edc853c84f4b65879a438d28b6a3dfc75
Praveen Choudhary <160106353+PraveenSirv@users.noreply.github.com>
2026-05-06 12:24:34 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/royal_medical/payment_service/exception/ExternalServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.royal_medical.payment_service.exception;
-
-
-public class ExternalServiceException extends RuntimeException {
-    public ExternalServiceException(String message) {
-        super(message);
-    }
-}
--- payment-service/src/main/java/com/royal_medical/payment_service/exception/InvalidRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.royal_medical.payment_service.exception;
-
-
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
