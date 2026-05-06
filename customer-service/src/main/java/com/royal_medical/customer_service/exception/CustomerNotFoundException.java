4632de9eec13d7f0e53da9a4f11dbc402ea3697f
Praveen Choudhary <160106353+PraveenSirv@users.noreply.github.com>
2026-05-06 12:24:54 +0530

    Delete customer-service directory

--- customer-service/src/main/java/com/royal_medical/customer_service/exception/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.royal_medical.customer_service.exception;
-
-public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
