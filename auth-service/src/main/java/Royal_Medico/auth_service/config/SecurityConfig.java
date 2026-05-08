820a771f873c63cc6e22cba87a6dd5f35333e729
Praveen <choudharypraveen856@gmail.com>
2026-05-08 13:12:05 +0530

    Test - 05/08/2026

--- auth-service/src/main/java/Royal_Medico/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/Royal_Medico/auth_service/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 );
